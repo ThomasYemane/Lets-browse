b4735eec2d5112a2786f1a5963d146545e8423f2
Thomas Yemane <tomblue246@gmail.com>
2025-08-16 15:06:31 -0700

    Phase 2: Signup thunk, page, and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage/LoginFormPage';
 import { restoreUser } from './store/session';
+import SignupFormPage from "./components/SignupFormPage/SignupFormPage";
+
 
 function Layout() {
   const dispatch = useDispatch();
@@ -20,7 +22,9 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { path: '/', element: <h1>Welcome!</h1> },
-      { path: '/login', element: <LoginFormPage /> }
+      { path: '/login', element: <LoginFormPage /> },
+      { path: "/signup", element: <SignupFormPage /> } 
+
     ]
   }
 ]);
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -38,4 +38,17 @@ export const restoreUser = () => async (dispatch) => {
   return res;
 };
 
+// SIGNUP thunk
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const res = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({ username, firstName, lastName, email, password })
+  });
+  const data = await res.json();
+  dispatch(setUser(data.user));
+  return res;
+};
+
+
 export default sessionSlice.reducer;
